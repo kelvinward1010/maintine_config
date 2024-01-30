42a452579b676e19f15c282ae3a0619effe747f3
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-30 12:59:17 +0700

    update

--- src/modules/mantine_core/background_image_config/BackgroundImageConfig.tsx
+++ src/modules/mantine_core/background_image_config/BackgroundImageConfig.tsx
@@ -1,19 +1,68 @@
-import { BackgroundImage, Box, Center, Text } from "@mantine/core";
+import { BackgroundImage, Box, Center, Grid, Text, Title } from "@mantine/core";
+import styles from './style.module.scss';
+import { CodeHighlightTabs } from "@mantine/code-highlight";
+import { TypeScriptIcon } from "@mantinex/dev-icons";
 
 export function BackgroundImageConfig() {
+
+    const tsIcon = <TypeScriptIcon size={15} />;
+
+    const contentCode1 =
+    `<BackgroundImage
+        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-6.png"
+        radius="sm"
+    >
+        <Center p="md">
+            <Text c="white">
+                BackgroundImage component can be used to add any content on image. It is useful for hero
+                headers and other similar sections
+            </Text>
+        </Center>
+    </BackgroundImage>`
+
     return (
-        <Box maw={'100%'} mx="auto">
-            <BackgroundImage
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-6.png"
-                radius="sm"
-            >
-                <Center p="md">
-                    <Text c="white">
-                        BackgroundImage component can be used to add any content on image. It is useful for hero
-                        headers and other similar sections
-                    </Text>
-                </Center>
-            </BackgroundImage>
-        </Box>
+        <div className={styles.container}>
+            <div className={styles.box_1}>
+                <Title order={5} c={'teal.5'}>1. Usage</Title>
+                <div className={styles.box_1_center}>
+                    <Grid grow>
+                        <Grid.Col span={6} miw={300}>
+                                <Title order={6} c={'teal.5'}>Code</Title>
+                                <CodeHighlightTabs
+                                    withExpandButton
+                                    defaultExpanded={false}
+                                    expandCodeLabel="Show full code"
+                                    collapseCodeLabel="Show less"
+                                    className={styles.code_highlight}
+                                    code={[
+                                        {
+                                            fileName: 'demo.tsx',
+                                            code: contentCode1,
+                                            language: 'tsx',
+                                            icon: tsIcon,
+                                        },
+                                    ]}
+                                />
+                        </Grid.Col>
+                        <Grid.Col span={6} miw={300}>
+                            <Title order={6} c={'teal.5'}>Demo</Title>
+                            <Box maw={'100%'} mx="auto">
+                                <BackgroundImage
+                                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-6.png"
+                                    radius="sm"
+                                >
+                                    <Center p="md">
+                                        <Text c="white">
+                                            BackgroundImage component can be used to add any content on image. It is useful for hero
+                                            headers and other similar sections
+                                        </Text>
+                                    </Center>
+                                </BackgroundImage>
+                            </Box>
+                        </Grid.Col>
+                    </Grid>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/modules/mantine_core/image_config/ImageConfig.tsx
+++ src/modules/mantine_core/image_config/ImageConfig.tsx
@@ -1,12 +1,58 @@
-import { Image } from "@mantine/core";
+import { Grid, Image, Title } from "@mantine/core";
+import styles from './style.module.scss';
+import { TypeScriptIcon } from "@mantinex/dev-icons";
+import { CodeHighlightTabs } from "@mantine/code-highlight";
 
 
 export function ImageConfig() {
+    const tsIcon = <TypeScriptIcon size={15} />;
+
+    const contentCode1 =
+    `<Image
+        radius="md"
+        h={200}
+        w="auto"
+        fit="contain"
+        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
+    />`
+
     return (
-        <Image
-            radius="md"
-            h={400}
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png"
-        />
+        <div className={styles.container}>
+            <div className={styles.box_1}>
+                <Title order={5} c={'teal.5'}>1. Usage</Title>
+                <div className={styles.box_1_center}>
+                    <Grid grow>
+                        <Grid.Col span={6} miw={300}>
+                            <Title order={6} c={'teal.5'}>Code</Title>
+                            <CodeHighlightTabs
+                                withExpandButton
+                                defaultExpanded={false}
+                                expandCodeLabel="Show full code"
+                                collapseCodeLabel="Show less"
+                                className={styles.code_highlight}
+                                code={[
+                                    {
+                                        fileName: 'demo.tsx',
+                                        code: contentCode1,
+                                        language: 'tsx',
+                                        icon: tsIcon,
+                                    },
+                                ]}
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={6} miw={300}>
+                            <Title order={6} c={'teal.5'}>Demo</Title>
+                            <Image
+                                radius="md"
+                                h={200}
+                                w="auto"
+                                fit="contain"
+                                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
+                            />
+                        </Grid.Col>
+                    </Grid>
+                </div>
+            </div>
+        </div>
     )
 }
